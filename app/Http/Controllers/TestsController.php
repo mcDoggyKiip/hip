b7e459b02733dd4832aa54d45add3ff1d8e8ea54
TomRoskam <tomroskam38@gmail.com>
2019-06-10 13:27:21 +0200

    Create controller and model

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -3,10 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tests;
 
 class TestsController extends Controller
 {
     public function index(){
-        return view('tests.index');
+        $tests = Tests::all();
+        return view('tests.index', compact('tests'));
+    }
+
+    public function create(){
+        return view('tests.create');
+    }
+
+    public function store(Request $request)
+    {
+        $tests = new tests();
+        $tests->attempt = request('attempt');
+        $tests->week = request('week');
+        $tests->save();
+
+        return redirect('/tests');
+    }
+
+    public function update(Request $request)
+    {
+        $tests = Tests::findOrFail($id);
+        $tests->attempt = request('attempt');
+        $tests->week = request('week');
+        $tests->save();
+
+        return redirect('/tests');
     }
 }
