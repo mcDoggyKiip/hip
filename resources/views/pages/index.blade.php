01b4d7cfb0562546aedbc59a357b5eb254d629bb
Glazelf <gideon.steenbergen@gmail.com>
2019-06-10 16:29:27 +0200

    pageIndex init

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,39 @@
+@extends('coreui::master')
+
+@section('body')
+    <div class="tablePages">
+        <a class="btn btn-success pagesAddButton" href={{ url('/pages/create') }}>Add new</a>
+        <p>Pages<p>
+        <table>
+            {{-- Head of table --}}
+            <tr>
+                <td>number</td>
+                <td>fileLocation</td>
+                <td>description</td>
+                <td>Actions</td>
+            </tr>
+
+            @foreach($pages as $row)
+            <tr>
+                <td class="bg-dark" style="width: 27vw;">{{ $row->number }}</td>
+                <td class="bg-dark" style="width: 27vw;">{{ $row->fileLocation }}</td>
+                <td class="bg-dark" style="width: 27vw;">{{ $row->description }}</td>
+                <td class="bg-dark">
+                    <div class="buttonsPages" style="width: 4vw;">
+                        <a style="float: left;margin: 0px 5px 0px 5px;" type="button" class="action-button btn btn-sm btn-primary" href="{{ url('pages/ . $row->id . /edit') }}">
+                            <i class="far fa-edit"></i>
+                        </a>
+                        <form method="post" action="{{ url('pages/' . $row->id) }}">
+                            @method('delete')
+                            @CSRF
+                            <button type="button" class="action-button btn btn-sm btn-danger">
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+@endsection('body')
